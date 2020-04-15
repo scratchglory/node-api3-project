54aab766f873afc9a989e0cd0e2777460860ba9e
adella <scratchglory@gmail.com>
2020-04-14 21:08:44 -0400

    added logger middleware

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code away!
+const express = require("express");
+const userRouter = require("./users/userRouter");
+// http request logger middleware for node.js
+const morgan = require("morgan");
+const cors = require("cors");
+const logger = require("./middleware/logger");
+
+// adding server
+const server = express();
+const port = 4040;
+
+// adding middleware
+server.use(express.json());
+// cross-origin resource sharing
+server.use(cors());
+// server.use(morgan())
+
+// short == never closing the request
+// long == closes and returns data
+server.use(logger("long"));
+
+// adding routers
+server.use("/users", userRouter);
+
+// Listening
+server.listen(port, () => {
+  console.log(`== Server On ${port} ==`);
+});
